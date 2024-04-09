928a6dfa9a4e6e61271195c7746982674f18241b
Nicholas Richards <nrichards@gmail.com>
2024-04-09 02:07:31 -0700

    Compositing named layers
    * Hacked together

--- psd-in-md.py
+++ psd-in-md.py
@@ -2,17 +2,33 @@ from psd_tools import PSDImage
 import os
 
 out_dirname = 'out'
-if  not os.path.exists(out_dirname):
+if not os.path.exists(out_dirname):
     os.mkdir(out_dirname)
 
 psd = PSDImage.open('test.psd')
 
 layer_names = ['SHIFT - s', 'DIAL - d', 'B5 - 5']
+# layer_names = ['B5 - 5']
+
+
+def match_layer(layer):
+    print(f'examining "{layer.name}"')
+    if layer.name:
+        if layer.name in layer_names:
+            print(f'matched, {layer.bbox}')
+            return True
+    elif layer.parent.kind == 'group' and layer.parent.name in layer_names:
+        print(f'matched, {layer.bbox}')
+        return True
+    return False
+
+# TODO crop to desired size
+
 
 image = psd.composite(
-    layer_filter=lambda layer: layer.is_visible() and layer.name in layer_names)
+    layer_filter=lambda layer: match_layer(layer))
 image.save(f'{out_dirname}/test.png')
-#
+
 # for layer in psd:
 #     print(layer)
 #     if layer.name != '':
