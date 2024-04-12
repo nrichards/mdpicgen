466b969cf255ab6dad66c0e26fb9f2572796a0eb
Nicholas Richards <nrichards@gmail.com>
2024-04-12 01:27:50 -0700

    Refine args. Convert core to class.

--- __main__.py
+++ __main__.py
@@ -1,29 +1,32 @@
-# Import necessary functions from psd_in_md.py
 from psd_in_md import format_markdown, process_psd
 
 if __name__ == '__main__':
     import argparse
 
-    parser = argparse.ArgumentParser(description="Process PSD file and format text as markdown")
-    parser.add_argument("--psd_file", type=str, help="Path to the PSD file")
-    parser.add_argument("--md_file", type=str, help="Input filename for the markdown file (default: stdout)")
-    parser.add_argument("--format", action="store_true", help="Format extracted text as markdown (default: False)")
+    parser = argparse.ArgumentParser(
+        description='''Read Markdown and process PSD, generating images and inserting / updating into Markdown.
+                    PSD layer names will be used as keys. 
+                    They will be matched to formatted key sequences found in Markdown tables with 
+                    first columns labelled \"Button\".
+                    Layers will be composited into images according to the sequences and saved. 
+                    Images will linked into Markdown in the second column, after the \"Button\" column. 
+                    Markdown will be dumped to stdout.
+                    ''')
+
+    parser.add_argument("md_file", type=str, help="Input filename for the Markdown file")
+
+    parser.add_argument("--psd_file", type=str, help="Input filename for the PSD file")
+    parser.add_argument("--psd_out_dir", default='out', type=str,
+                        help="Output directory name for composited images, will be created (Default: 'out')")
+
     args = parser.parse_args()
 
-    # Handle missing arguments gracefully
-    if not any(vars(args).values()):
-        parser.print_help()
-        exit(1)
-
-    # Process the PSD file
     if args.psd_file:
         try:
-            process_psd(args.psd_file)
+            process_psd(args.psd_out_dir, args.md_file, args.psd_file)
         except Exception as e:
             print(f"Error processing PSD file: {e}")
             exit(1)
 
-    # Optionally format the extracted text
-    if args.format:
-        formatted_text = format_markdown(args.md_file)
-        print(formatted_text)
+    formatted_text = format_markdown(args.md_file)
+    print(formatted_text)
--- psd_in_md.py
+++ psd_in_md.py
@@ -13,105 +13,105 @@ layer_names: [str] = ['SHIFT - s', 'SEQ PLAY - splay', 'DIAL - d']
 
 full_layer_names: [str] = layer_names + [BG_LAYER_NAME]
 
-bbox = None
 
-"BG"
-"LOOPER STOP - ls"
-"LOOPER PLAY - lplay"
-"LOOPER REC - lr"
-"PARAM - param"
-"SYSTEM - sys"
-"MODE PLAY - mplay"
-"SEQ PLAY - splay"
-"B8 - 8"
-"B7 - 7"
-"B6 - 6"
-"B5 - 5"
-"B4 - 4"
-"B3 - 3"
-"B2 - 2"
-"B1 - 1"
-"NO - n"
-"OK - o"
-"SHIFT - s"
-"DIAL - d"
-
-out_dirname = 'out'
-
-
-def make_out_dir():
+def make_out_dir(out_dirname):
     if not os.path.exists(out_dirname):
         os.mkdir(out_dirname)
 
 
-def layer_name_from_text(button_name):
-    # TODO build a dictionary mapping words used in the Qun manual to layer names
-    pass
+def process_psd(out_dirname, md_file, psd_filename):
+    PSDInMd().process_psd(out_dirname, md_file, psd_filename)
 
 
-def match_layer(layer, match_names):
-    print(f'inspecting \"{layer.name}\"')
-    if layer.name:
-        if layer.name in match_names:
+class PSDInMd:
+    bbox = None
+
+    "BG"
+    "LOOPER STOP - ls"
+    "LOOPER PLAY - lplay"
+    "LOOPER REC - lr"
+    "PARAM - param"
+    "SYSTEM - sys"
+    "MODE PLAY - mplay"
+    "SEQ PLAY - splay"
+    "B8 - 8"
+    "B7 - 7"
+    "B6 - 6"
+    "B5 - 5"
+    "B4 - 4"
+    "B3 - 3"
+    "B2 - 2"
+    "B1 - 1"
+    "NO - n"
+    "OK - o"
+    "SHIFT - s"
+    "DIAL - d"
+
+    # out_dirname = 'out'
+    psd = None
+
+    def layer_name_from_text(self, button_name):
+        # TODO build a dictionary mapping words used in the Qun manual to layer names
+        pass
+
+    def match_layer(self, layer, match_names):
+        print(f'inspecting \"{layer.name}\"')
+        if layer.name:
+            if layer.name in match_names:
+                print(f'matched {layer.name}, {layer.bbox}')
+                return True
+            elif layer.parent is not None and layer.parent.kind == 'group' and layer.parent.name in match_names:
+                return True
+        elif layer.parent.kind == 'group' and layer.parent.name in match_names:
             print(f'matched {layer.name}, {layer.bbox}')
             return True
-        elif layer.parent is not None and layer.parent.kind == 'group' and layer.parent.name in match_names:
-            return True
-    elif layer.parent.kind == 'group' and layer.parent.name in match_names:
-        print(f'matched {layer.name}, {layer.bbox}')
-        return True
-    else:
-        return match_layer(layer.parent, match_names)
-        # False
-    # elif match_layer(layer.parent, match_names):
-    #     return True
-    return False
+        else:
+            return self.match_layer(layer.parent, match_names)
+            # False
+        # elif match_layer(layer.parent, match_names):
+        #     return True
+        return False
 
+    def find_bbox(self):
+        global bbox
+        for layer in self.psd.descendants():
+            print(layer)
+            if BG_LAYER_NAME == layer.name:
+                bbox = layer.bbox
+                break
+        if bbox is None:
+            print(f"Warning: Bounding box layer {BG_LAYER_NAME} not found. Output images will be full size.")
+            bbox = self.psd.bbox
 
-def find_bbox():
-    global bbox
-    for layer in psd.descendants():
-        print(layer)
-        if BG_LAYER_NAME == layer.name:
-            bbox = layer.bbox
-            break
-    if bbox is None:
-        print(f"Warning: Bounding box layer {BG_LAYER_NAME} not found. Output images will be full size.")
-        bbox = psd.bbox
+    def gen_image_name(self, names):
+        image_name = ''
 
+        for name in names:
+            words = name.split()
+            chunk = words[-1].strip()
+            print(f'chunk {chunk}')
+            if image_name != '':
+                image_name += "_"
+            image_name += chunk
 
-def gen_image_name(names):
-    image_name = ''
+        return image_name
 
-    for name in names:
-        words = name.split()
-        chunk = words[-1].strip()
-        print(f'chunk {chunk}')
-        if image_name != '':
-            image_name += "_"
-        image_name += chunk
+    def process_psd(self, out_dirname, md_file, psd_filename):
+        make_out_dir(out_dirname)
 
-    return image_name
+        self.psd = PSDImage.open(psd_filename)
+        self.find_bbox()
+        image = self.psd.composite(
+            viewport=bbox,
+            layer_filter=lambda candidate_layer: self.match_layer(candidate_layer, full_layer_names))
+        new_height = 48
+        reduction_scalar = new_height / image.size[1]
+        new_width = int(reduction_scalar * image.size[0])
+        resized_image = image.resize([new_width, new_height])
+        resized_image.save(f'{out_dirname}/{self.gen_image_name(layer_names)}.png')
 
-
-def process_psd(psd_filename):
-    global psd
-    make_out_dir()
-    psd = PSDImage.open(psd_filename)
-    find_bbox()
-    image = psd.composite(
-        viewport=bbox,
-        layer_filter=lambda candidate_layer: match_layer(candidate_layer, full_layer_names))
-    new_height = 48
-    reduction_scalar = new_height / image.size[1]
-    new_width = int(reduction_scalar * image.size[0])
-    resized_image = image.resize([new_width, new_height])
-    resized_image.save(f'{out_dirname}/{gen_image_name(layer_names)}.png')
-
-
-
-# for layer in psd:
-#     print(layer)
-#     if layer.name != '':
-#         layer_image = layer.composite()
-#         layer_image.save('out/%s.png' % layer.name)
+    # for layer in psd:
+    #     print(layer)
+    #     if layer.name != '':
+    #         layer_image = layer.composite()
+    #         layer_image.save('out/%s.png' % layer.name)
