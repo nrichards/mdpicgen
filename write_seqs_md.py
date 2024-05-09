e869b4064b309822fb2c387796cc328af796bd76
Nicholas Richards <nrichards@gmail.com>
2024-05-09 11:50:59 -0700

    Fix partial description in table

--- write_seqs_md.py
+++ write_seqs_md.py
@@ -217,13 +217,8 @@ class WriteSequences:
             combo:Combo
             for combo in section.combos:
                 rows.append(f"{combo.seq.text}<br>{combo.seq.md_image_link(image_out_path, opt)}"
-                            f"<br>{combo.seq.description}")
-            # columns = ["SHIFT + B1 <br> ![label-1.1](link-1.1) <br> Cell 1.1 text",
-            #            "SHIFT + B2 <br> ![label-2.1](link-2.1) <br> Cell 2.1 text",
-            #            "SHIFT + B3 <br> ![label-3.1](link-3.1) <br> Cell 3.1 text"]
-            # rows = [columns[0],
-            #         "SHIFT + B1 <br> ![label-1.2](link-1.2) <br> Cell 1.2 text",
-            #         "SHIFT + B1 <br> ![label-1.3](link-1.3) <br> Cell 1.3 text"]
+                            f"<br>{combo.seq.description_printable()}")
+                
             row_text = WriteSequences.to_table_line(rows)
             table_text = header_text + row_text
 
